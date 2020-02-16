92abbbe1a21566f9a4b60dce5cbfcedb59632d6d
Seyh Zamani <zamani.seyh@gmail.com>
2020-02-16 15:16:31 -0800

    update player

--- src/models/player.js
+++ src/models/player.js
@@ -1,24 +1,52 @@
  const { PlayerLostEvent } = require('../models/events/player-lost-event');
  const { PlayerWonEvent } = require('../models/events/player-won-event');
  const { PlayerJoinedToGameEvent } = require('../models/events/player-joined-to-game-event');
-
-
+ const { PlayerActivatedEvent } = require('../models/events/player-activated-event');
+ const { PlayerCreatedEvent } = require('../models/events/player-created-event');
+ const { PlayerDeactivatedEvent } = require('../models/events/player-deactivated-event');
+ const { PlayerDepositCreditEvent } = require('../models/events/player-deposit-credit-event');
+ const { PlayerWithdrawCreditEvent } = require('../models/events/player-withdraw-credit-event');
 
  class Player {
+     constructor() {
+         this.playerId = null;
+         this.active = false;
+         this.credit = 0;
+     }
+
+     hasEnoughCredit(betAmount) {
+         return this.credit >= betAmount;
+     }
+
      apply(events) {
          if (!Array.isArray(events) || events.length === 0) {
              throw new Error('Player Events should be array and be greater 0');
          }
          for (const event of events) {
              switch (event.constructor) {
-                 case PlayerJoinedToGameEvent:
-                     this.applyGameCreatedEvent(event);
+                 case PlayerActivatedEvent:
+                     this.applyPlayerActivatedEvent(event);
                      break;
-                 case PlayerWonEvent:
-                     this.applyDiceRolledEvent(event);
+                 case PlayerCreatedEvent:
+                     this.applyPlayerCreatedEvent(event);
+                     break;
+                 case PlayerDeactivatedEvent:
+                     this.applyPlayerDeactivatedEvent(event);
+                     break;
+                 case PlayerDepositCreditEvent:
+                     this.applyPlayerDepositCreditEvent(event);
+                     break;
+                 case PlayerJoinedToGameEvent:
+                     this.applyPlayerJoinedToGameEvent(event);
                      break;
                  case PlayerLostEvent:
                      this.applyGameFinishedEvent(event);
+                     break;
+                 case PlayerWithdrawCreditEvent:
+                     this.applyPlayerWithdrawCreditEvent(event);
+                     break;
+                 case PlayerWonEvent:
+                     this.applyDiceRolledEvent(event);
                      break;
                  default:
                      throw new Error('Unknown Event!');
@@ -26,12 +54,35 @@
          }
      }
 
-     process(command) {
+     applyPlayerActivatedEvent() {
+         this.active = true;
+     }
+     applyPlayerCreatedEvent(playerCreatedEvent) {
+         const { credit } = playerCreatedEvent.getMetadata();
+         this.active = true;
+         this.credit += credit;
+     }
+     applyPlayerDeactivatedEvent() {
+         this.active = false;
+     }
+     applyPlayerDepositCreditEvent(playerDepositCreditEvent) {
+         const { credit } = playerDepositCreditEvent.getMetadata();
+         this.credit += credit;
+     }
+     applyPlayerJoinedToGameEvent(playerJoinedToGameEvent) {
+         const { credit } = playerJoinedToGameEvent.getMetadata();
+         this.credit += credit;
+     }
+     applyPlayerLostEvent() {
 
      }
-
-     hasEnoughCredit(betAmount) {
-         return true;
+     applyPlayerWithdrawCreditEvent(playerWithdrawCreditEvent) {
+         const { credit } = playerWithdrawCreditEvent.getMetadata();
+         this.credit += credit;
+     }
+     applyPlayerWonEvent(playerWonEvent) {
+         const { credit } = playerWonEvent.getMetadata();
+         this.credit += credit;
      }
  }
 
